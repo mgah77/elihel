3d935d15ce4cd57ad532982e3e8ffd40e514a147
mgah77 <138524599+mgah77@users.noreply.github.com>
2024-10-15 16:49:23 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-{ 'name': 'Insumar_taller',
-'summary': "OT y certificaciones",
+{ 'name': 'Elihel',
+'summary': "Trabajos",
 'author': "Mauricio Gah",
 'license': "AGPL-3",
 'application': "True",
