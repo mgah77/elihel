7cdd911c59a0e9bbafc11d2dc07c308efbe695c3
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-02-03 20:16:27 -0300

    Update barco.py

--- models/barco.py
+++ models/barco.py
@@ -4,7 +4,7 @@ class Barco(models.Model):
     _name = 'elihel.barco'
     _description = 'Barco'
 
-    name = fields.Char(string='Matrícula', required=True)  # Matrícula del barco
-    valor = fields.Float(string='Valor')  # Valor asociado al barco
+    matricula = fields.Char(string='Matrícula', required=True)  # Matrícula del barco
+    valor = fields.Float(string='Valor', required=True)  # Valor del barco
     fecha_llegada = fields.Datetime(string='Fecha de Llegada', default=fields.Datetime.now)  # Fecha de llegada
-    camion_ids = fields.One2many('elihel.camion', 'barco_id', string='Camiones')  # Camiones asociados al barco
+    camion_ids = fields.One2many('elihel.camion', 'barco_id', string='Camiones')  # Camiones asociados
