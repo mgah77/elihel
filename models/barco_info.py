0ca0a2d767b3b4d0472011bd5e45158197d6e3c3
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-02-03 22:40:44 -0300

    34

--- models/barco_info.py
+++ models/barco_info.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class BarcoInfo(models.Model):
     _name = 'elihel.barco_info'
@@ -6,5 +7,13 @@ class BarcoInfo(models.Model):
     _rec_name = 'nombre'  # Define el campo 'nombre' como el nombre predeterminado
 
     nombre = fields.Char(string='Nombre del Barco', required=True)  # Nombre del barco
-    matricula = fields.Char(string='Matrícula del Barco', required=True)  # Matrícula del barco
+    matricula = fields.Char(string='Matrícula del Barco', required=True, size=6)  # Matrícula del barco (6 caracteres)
     valor = fields.Float(string='Valor del Barco', required=True)  # Valor del barco
+
+    @api.constrains('matricula')
+    def _check_matricula(self):
+        for record in self:
+            if len(record.matricula) != 6:
+                raise ValidationError("La matrícula debe tener exactamente 6 caracteres.")
+            # Convertir a mayúsculas
+            record.matricula = record.matricula.upper()
--- models/camion.py
+++ models/camion.py
@@ -1,9 +1,18 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Camion(models.Model):
     _name = 'elihel.camion'
     _description = 'Camión'
 
-    matricula = fields.Char(string='Matrícula', required=True)  # Matrícula del camión
+    matricula = fields.Char(string='Matrícula del Camión', required=True, size=6)  # Matrícula del camión (6 caracteres)
     barco_id = fields.Many2one('elihel.barco', string='Barco', ondelete='cascade')  # Relación con el barco
     servicio_ids = fields.One2many('elihel.servicio', 'camion_id', string='Servicios')  # Servicios asociados
+
+    @api.constrains('matricula')
+    def _check_matricula(self):
+        for record in self:
+            if len(record.matricula) != 6:
+                raise ValidationError("La matrícula debe tener exactamente 6 caracteres.")
+            # Convertir a mayúsculas
+            record.matricula = record.matricula.upper()
