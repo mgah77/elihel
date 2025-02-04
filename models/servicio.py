0c82a9e523741d98b3f6409bed1453c2229797d7
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-02-03 22:36:05 -0300

    3434

--- models/servicio.py
+++ models/servicio.py
@@ -3,13 +3,12 @@ from odoo import models, fields
 class Servicio(models.Model):
     _name = 'elihel.servicio'
     _description = 'Servicio'
+    _rec_name = 'tipo_servicio'  # Define el campo 'tipo_servicio' como el nombre predeterminado
 
-    # Campo Selection para el tipo de servicio
     tipo_servicio = fields.Selection([
         ('desinfeccion', 'Desinfección'),
         ('reffers', 'Reffers'),
         ('bins', 'Bins'),
-    ], string='Tipo de Servicio', required=True)
-
+    ], string='Tipo de Servicio', required=True)  # Tipo de servicio
     cantidad = fields.Integer(string='Cantidad', required=True)  # Cantidad del servicio
     camion_id = fields.Many2one('elihel.camion', string='Camión', ondelete='cascade')  # Relación con el camión
