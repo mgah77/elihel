64e6d2ac5f7e39f80d55c285f9f951c1c7589d41
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-02-04 22:14:35 -0300

    qwe

--- models/barco.py
+++ models/barco.py
@@ -6,8 +6,12 @@ class Barco(models.Model):
     _rec_name = 'nombre'  # Define el campo 'nombre' como el nombre predeterminado
 
     barco_info_id = fields.Many2one('elihel.barco_info', string='Información del Barco', required=True)  # Relación con BarcoInfo
-    fecha_llegada = fields.Datetime(string='Fecha de Llegada', default=fields.Datetime.now)  # Fecha de llegada
+    fecha_llegada = fields.Datetime(string='Fecha', default=fields.Datetime.now)  # Fecha
     camion_ids = fields.One2many('elihel.camion', 'barco_id', string='Camiones')  # Camiones asociados
+    lugar = fields.Selection([
+        ('pue', 'Puerto Montt'),
+        ('cco', 'Chacabuco'),
+    ], string='Lugar', required=True)  # Lugar de trabajo      
 
     # Campos relacionados (para mostrar en vistas)
     nombre = fields.Char(related='barco_info_id.nombre', string='Nombre', readonly=True)  # Nombre del barco
