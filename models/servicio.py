8f38f3e463017000a8c37cd626442e41bc86dc36
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-04-01 11:39:34 -0300

    Update servicio.py

--- models/servicio.py
+++ models/servicio.py
@@ -80,11 +80,15 @@ class PrecioServicio(models.Model):
         required=True,
         default=0
     )
-      
+
     descripcion = fields.Char(
         string='Descripción',
-        related='tipo_servicio',
-        readonly=True
+        compute='_compute_descripcion',
+        store=True
     )
+    
+    @api.depends('tipo_servicio')
+    def _compute_descripcion(self):
+        return
     
     
